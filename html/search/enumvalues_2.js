1d6e852dcf27b377703c423f5facb9e38095dfa2
unvirus <83897755+unvirus@users.noreply.github.com>
2023-12-11 10:32:29 +0900

    Add files via upload

---
+++ html/search/enumvalues_2.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['watch_5fpoint_5fbyte_0',['WATCH_POINT_BYTE',['../group__dbgctrl.html#ggacd0a4168bef2892463390ec7c40b3af3a5ae5d9eeb327ba2da47bc44f8a3dd40d',1,'dbgctrl.h']]],
+  ['watch_5fpoint_5fdword_1',['WATCH_POINT_DWORD',['../group__dbgctrl.html#ggacd0a4168bef2892463390ec7c40b3af3a3636f8fa37b3451dafd28c2978c212fb',1,'dbgctrl.h']]],
+  ['watch_5fpoint_5fqword_2',['WATCH_POINT_QWORD',['../group__dbgctrl.html#ggacd0a4168bef2892463390ec7c40b3af3a3320dddb891c6a4848e9559caacc0586',1,'dbgctrl.h']]],
+  ['watch_5fpoint_5fread_3',['WATCH_POINT_READ',['../group__dbgctrl.html#gga1d9128302618cb4dc1ad6712e7758b69a4471b2253588d06acc57c86b60e60fdc',1,'dbgctrl.h']]],
+  ['watch_5fpoint_5frw_4',['WATCH_POINT_RW',['../group__dbgctrl.html#gga1d9128302618cb4dc1ad6712e7758b69a0f804c4c61c207732029142814e96ac3',1,'dbgctrl.h']]],
+  ['watch_5fpoint_5fword_5',['WATCH_POINT_WORD',['../group__dbgctrl.html#ggacd0a4168bef2892463390ec7c40b3af3a7ba86e6282ff5213449351d78c49006f',1,'dbgctrl.h']]],
+  ['watch_5fpoint_5fwrite_6',['WATCH_POINT_WRITE',['../group__dbgctrl.html#gga1d9128302618cb4dc1ad6712e7758b69aaba6c280ad5266d4c6e201a6a5420244',1,'dbgctrl.h']]]
+];
---
+++ html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dbgctrl_2eh_0',['dbgctrl.h',['../dbgctrl_8h.html',1,'']]]
+];
