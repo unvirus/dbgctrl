67ed082b5ec9a9b83ce7b270a22c58db81916256
unvirus <83897755+unvirus@users.noreply.github.com>
2024-02-20 10:59:38 +0900

    Add files via upload

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,7 +1,13 @@
 var searchData=
 [
-  ['evt_5fbreak_5fpoint_0',['EVT_BREAK_POINT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a22b26075cc87db9229a86cb0335e7e2b',1,'dbgctrl.h']]],
-  ['evt_5ftarget_5fconnect_1',['EVT_TARGET_CONNECT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a997c55781b6169cba76db9f6decfdf40',1,'dbgctrl.h']]],
-  ['evt_5ftarget_5fdisconnect_2',['EVT_TARGET_DISCONNECT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21ae94a7f5e45c7eacb63f1a597d793f448',1,'dbgctrl.h']]],
-  ['evt_5fwatch_5fpoint_3',['EVT_WATCH_POINT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a3f80e4793a0f6e201475874c631ac8d2',1,'dbgctrl.h']]]
+  ['evt_5falignment_5ffault_0',['EVT_ALIGNMENT_FAULT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21aad2bc5adb298026745d1f9c8937bf06a',1,'dbgctrl.h']]],
+  ['evt_5fbreak_5fpoint_1',['EVT_BREAK_POINT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a22b26075cc87db9229a86cb0335e7e2b',1,'dbgctrl.h']]],
+  ['evt_5fdata_5fabort_2',['EVT_DATA_ABORT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21ad6c829390f4e7f048bd676dddcd70c06',1,'dbgctrl.h']]],
+  ['evt_5fpause_3',['EVT_PAUSE',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21acd45382e94420dc81605bbb10f1463c8',1,'dbgctrl.h']]],
+  ['evt_5fprefetch_5fabort_4',['EVT_PREFETCH_ABORT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a8635bad0c010a61e2f50ee5d0b1f2d92',1,'dbgctrl.h']]],
+  ['evt_5fsw_5fbreak_5fpoint_5',['EVT_SW_BREAK_POINT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21ae247528d8fb4f73ef4c0b2372ce23330',1,'dbgctrl.h']]],
+  ['evt_5ftarget_5fconnect_6',['EVT_TARGET_CONNECT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a997c55781b6169cba76db9f6decfdf40',1,'dbgctrl.h']]],
+  ['evt_5ftarget_5fdisconnect_7',['EVT_TARGET_DISCONNECT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21ae94a7f5e45c7eacb63f1a597d793f448',1,'dbgctrl.h']]],
+  ['evt_5ftrap_8',['EVT_TRAP',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a6c299b8db1b6274576ab3d8dae488777',1,'dbgctrl.h']]],
+  ['evt_5fwatch_5fpoint_9',['EVT_WATCH_POINT',['../group__dbgctrl.html#gga8fbaa92b672b110f3adc007ab2782b21a3f80e4793a0f6e201475874c631ac8d2',1,'dbgctrl.h']]]
 ];
